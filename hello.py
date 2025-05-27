5a9fed7d34eb5f3b3c12679d78260d0be2c8b436
soumikchandra <shibmunchandra@gmail.com>
2025-05-27 13:18:29 +0530

    Fisrt Python code

---
+++ hello.py
@@ -0,0 +1,12 @@
+def factorial(n):
+	if n==0:
+		return 1
+	elif(n<0):
+		return 0
+	else:
+		return n*factorial(n-1)
+		
+number=5
+result=factorial(number)
+print(f"The factorial of {number} is {result}")
+
